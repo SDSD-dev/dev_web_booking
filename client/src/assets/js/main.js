6ceeaa03068ed9887821c6ff8c7307904460adda
SDSD-dev <sdsdgit@free.fr>
2026-04-07 09:53:44 +0200

    fix: restauration du script menu burger mobile sur Angular

--- client/src/assets/js/main.js
+++ client/src/assets/js/main.js
@@ -1,5 +1,17 @@
 // src/assets/js/main.js
 document.addEventListener('DOMContentLoaded', () => {
 
+  /* ------------------------ */
+  /* ---   MENU BURGER    --- */
+  /* ------------------------ */
+  const burgerBtn = document.getElementById('burger-btn');
+  const mobileMenu = document.getElementById('mobile-menu');
+
+  if (burgerBtn && mobileMenu) {
+    burgerBtn.addEventListener('click', () => {
+      mobileMenu.classList.toggle('is-open');
+    });
+  }
+
 });
 
