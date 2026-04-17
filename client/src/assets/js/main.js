2a5161f8ad8ee96e667674d7d765b3a1dfda495e
SDSD-dev <sdsdgit@free.fr>
2026-04-17 16:04:11 +0200

    feat : ajout d'un 'effet sticky header'
    - UI : amélioration de l'affichage mobile de profile
    - fix : problème d'affichage du menu burger

--- client/src/assets/js/main.js
+++ client/src/assets/js/main.js
@@ -1,17 +1,25 @@
-// src/assets/js/main.js
 document.addEventListener('DOMContentLoaded', () => {
 
-  /* ------------------------ */
-  /* ---   MENU BURGER    --- */
-  /* ------------------------ */
-  const burgerBtn = document.getElementById('burger-btn');
-  const mobileMenu = document.getElementById('mobile-menu');
+  /* --------------------------------
+     EFFET STICKY HEADER
+     -------------------------------- */
+  // cible -> barre supérieure du header
+  const topBar = document.querySelector('.top-bar');
 
-  if (burgerBtn && mobileMenu) {
-    burgerBtn.addEventListener('click', () => {
-      mobileMenu.classList.toggle('is-open');
+  if (topBar) {
+    // On écoute l'événement de défilement (scroll) de la page
+    window.addEventListener('scroll', () => {
+      
+      // Si on descend de plus de 50 pixels
+      if (window.scrollY > 50) {
+        // On ajoute une classe pour l'ombre
+        topBar.classList.add('scrolled-shadow');
+      } else {
+        // Sinon on la retire (retour tout en haut)
+        topBar.classList.remove('scrolled-shadow');
+      }
+      
     });
   }
 
 });
-
